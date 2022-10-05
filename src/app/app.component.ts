9b120a8f74625efa8ca7d6e8e78f3d6aa3cd6c84
Gurunadh552 <gurunadhpukkalla@gmail.com>
2022-10-05 21:37:51 +0530

    Login Form in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'login-themes';
 
-  COLORS = [
-    "#1DD1A1",
-    "#FF6B6B",
-    "#2E86DE",
-    "#F368E0",
-    "#FF9F43"
-  ] as const;
+  COLORS = ['#1DD1A1', '#FF6B6B', '#2E86DE', '#F368E0', '#FF9F43'] as const;
+
+  activeColor : string = this.COLORS[0];
+
+  changeColor(i : string ){
+    document.body.style.backgroundColor = i;
+    this.activeColor = i;
+  }
 }
